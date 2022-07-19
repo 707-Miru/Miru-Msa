2d3c7c64fc7062d5221f0a7cbbd2a1cdc5652cfa
정태영 <jty419@naver.com>
2022-07-19 23:51:03 +0900

    0719 TY Add miru skeleton code

---
+++ src/main/java/com/back/miru/model/dao/UserDAO.java
@@ -0,0 +1,29 @@
+package com.back.miru.model.dao;
+
+import com.back.miru.model.dto.User;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+
+public interface UserDAO {
+
+	public User login(String id, String password) throws SQLException;
+
+	public int idCheck(String id) throws SQLException;
+
+	public int passwordFindCheck(Uconcern.xmlser user) throws SQLException;
+	
+	public int passwordCheck(Map<String, String> map) throws SQLException;
+
+	public void registerUser(Map<String, String> map) throws SQLException;
+
+	public void updateUser(Map<String, String> map) throws SQLException;
+
+	public void pwUpdate(Map<String, String> map);
+	
+	public void deleteUser(String id) throws SQLException;
+
+	public User infoUser(String id) throws SQLException;
+
+}
